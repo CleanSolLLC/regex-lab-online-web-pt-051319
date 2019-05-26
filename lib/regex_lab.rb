e7ded934363713e486bbcaa34500dee55184bdbd
Mark May <mamark90@gmail.com>
2019-05-26 22:59:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,29 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  if word.match(/\b[AEIOUaeiou]/)
+    return true
+  elsif word.match(/\b[^\sAEIOUaeiou]/)
+    #binding.pry
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
+  if text.scan(/\b[A-Z] && [.]/)
+    return true
+  elsif text.scan(/\b[a-z] && [.]/)
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
